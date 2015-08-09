45e07d55964880eeaac8500ec4c9d09ba99abda4
spazzarama <spazzarama@gmail.com>
2015-08-09 18:14:05 +1000

    Minor fixes to build.proj for GitHub repository

--- build-package.bat
+++ build-package.bat
@@ -2,5 +2,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 msbuild build.proj /t:Clean;BeforeBuild
 msbuild build.proj /t:Build
-msbuild build.proj /t:ExportSource;PreparePackage;Package
+msbuild build.proj /t:PreparePackage;Package
 pause
